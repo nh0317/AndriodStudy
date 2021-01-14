696fc2c46e1ba483f3b42e771892aca6155ed315
KNH <nh2004@naver.com>
2021-01-15 00:46:19 +0900

    ImageButton
    이미지 버튼 연습

---
+++ ImageButton1/app/src/main/java/kr/co/company/imagebutton1/MainActivity.java
@@ -0,0 +1,20 @@
+package kr.co.company.imagebutton1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View target){
+        Toast.makeText(getApplicationContext(),"버튼이 눌러졌습니다",Toast.LENGTH_SHORT).show();
+    }
+}
