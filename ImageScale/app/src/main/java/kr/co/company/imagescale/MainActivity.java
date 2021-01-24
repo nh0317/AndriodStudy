25b4e9ad231998ca8c170c2cfc5a1f0197172ede
KNH <nh2004@naver.com>
2021-01-24 21:40:54 +0900

    ImageScale
    머티 터치를 이요하여
    이미지를 확대하는 예제

---
+++ ImageScale/app/src/main/java/kr/co/company/imagescale/MainActivity.java
@@ -0,0 +1,15 @@
+package kr.co.company.imagescale;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new MyImageView(this));
+    }
+}
