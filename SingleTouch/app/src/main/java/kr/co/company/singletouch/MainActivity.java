c208a2c3f67f5b7ce4e573982b956611b68ff231
KNH <nh2004@naver.com>
2021-01-24 21:45:52 +0900

    SingleTouch
    터치로 그림을 그리는 예제

---
+++ SingleTouch/app/src/main/java/kr/co/company/singletouch/MainActivity.java
@@ -0,0 +1,15 @@
+package kr.co.company.singletouch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new SingleTouchView(this,null));
+    }
+}
